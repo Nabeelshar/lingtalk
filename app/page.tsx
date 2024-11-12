e5dccf0f9ae9a67ab94372f9ed0b6aad383b48bf
Nab <sharyt26shar@gmail.com>
2024-11-12 18:07:44 +0500

    fix styles

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       {user ? <Chat user={user} /> : <Login />}
     </main>
   );
